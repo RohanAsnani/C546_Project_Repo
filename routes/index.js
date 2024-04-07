d122d5f911679ea917bc845edde0565487202557
SohamX <sohamtalekar7@gmail.com>
2024-04-07 00:20:30 -0400

    added CRUD folders to routes and data functions. downloaded nondemon for automatic server restart on any saved file changes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 //Here you will import route files and export them as used in previous labs
-import user_Post from './user_Post.js';
+import user_Test from './user_Test.js';
 import path from 'path';
 import {static as staticDir} from 'express';
 
 const constructorMethod = (app) => {
-    app.use('/', user_Post);
+    app.use('/', user_Test);
     app.use('/public', staticDir('public'));
     app.use('*', (req, res) => {
       res.redirect('/');
