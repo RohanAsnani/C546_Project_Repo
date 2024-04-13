f64075cf8860d1dbf89d0caaa7f68464e72ac4f1
NishankShetty <nishankshetty2001@gmail.com>
2024-04-13 12:48:58 -0400

    index.js

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ import path from 'path';
 import {static as staticDir} from 'express';
 
 const constructorMethod = (app) => {
-    app.use('/', user_Test);
+    app.use('/users', user_Test);
     app.use('/public', staticDir('public'));
     app.use('*', (req, res) => {
       res.redirect('/');
